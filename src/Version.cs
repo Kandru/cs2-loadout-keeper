0ceceabee28f76d2e00ee5275d3b2a8b420de5a3
derkalle4 <kalle@kandru.de>
2025-08-13 15:08:56 +0200

    bump version to 1.0.4

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace LoadoutKeeper
 {
     public partial class LoadoutKeeper
     {
-        public override string ModuleVersion => "1.0.3";
+        public override string ModuleVersion => "1.0.4";
     }
 }
